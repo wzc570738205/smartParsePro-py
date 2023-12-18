1cdd8632ecbbb21fb5226577f12d17f82b5f4cbd
wangzhichao <570738205@qq.com>
2023-12-18 12:00:58 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# -smartParsePro-py-
+# smartParsePro-py
 地址解析识别python版本
+项目基于[JioNLP 地址解析](https://github.com/dongrixinyu/JioNLP/wiki/Gadget-%E8%AF%B4%E6%98%8E%E6%96%87%E6%A1%A3#user-content-%E5%9C%B0%E5%9D%80%E8%A7%A3%E6%9E%90)来进行地址识别
+
+地址库：[2020年国家统计局行政区划](http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2020)
+
+基于[结巴分词](https://github.com/fxsjy/jieba) 提供的分词以及语义化分析进行姓名识别
+
+基于[Levenshtein](https://github.com/Levenshtein) 字符串相似度算法来进行详细地址过滤
+
+# 依赖下载
+python支持版本：`>=3.8`
+
+JioNLP下载
+```bash
+pip3 install jionlp
+```
+结巴分词下载
+```bash
+pip3 install jieba
+```
+Levenshtein字符串相似度分析
+```bash
+pip3 install Levenshtein
+```
+paddlepaddle语义分析参考[paddlepaddle 3.8 支持](https://github.com/fxsjy/jieba/issues/920)
+```bash
+pip3 install paddlepaddle==2.4.2
+```
+flask下载：
+```bash
+pip3 install flask
+```
+
+# 使用
+直接运行
+```bash
+python3 index.py '王志超029-68216000新疆维吾尔自治区乌鲁木齐市沙依巴克区西虹东路463号'
+```
+接口形式运行 
+```bash
+# 默认端口3000
+python3 index.py serve  
+# 指定端口运行
+python3 index.py serve 8088 
+```
+## api调用
+```JSON
+URL: `http://127.0.0.1:3000/smart_address`
+METHOD: 'POST'
+BODY:`
+{
+    "text":"王志超029-68216000新疆维吾尔自治区乌鲁木齐市沙依巴克区西虹东路463号",
+    "town_village": true, //可不传默认true 指定参数town_village(bool)，可获取乡镇、村、社区两级详细地名 
+    "change2new": false //可不传默认false 指定参数change2new(bool)可自动将旧地址转换为新地址
+}`
+RESPONSE:`
+{
+    "city": "乌鲁木齐市",
+    "county": "沙依巴克区",
+    "detail": "西虹东路463号",
+    "name": "王志超",
+    "phone": "029-68216000",
+    "province": "新疆维吾尔自治区",
+    "town": "",
+    "village": ""
+}`
+```
---
+++ index.py
@@ -0,0 +1,156 @@
+# encoding=utf-8
+import jieba
+import paddle
+import jionlp as jio
+import jieba.posseg as pseg
+import re
+import Levenshtein
+from flask import Flask, request, jsonify
+import sys
+
+paddle.enable_static()
+jieba.enable_paddle()
+
+# 地址识别 
+# address_string 需要识别的字符串
+# town_village   指定参数town_village(bool)，可获取乡镇、村、社区两级详细地名
+# change2new     指定参数change2new(bool)可自动将旧地址转换为新地址
+def smart_address(address_string,town_village=True, change2new=False):
+  obj = {
+    'phone': "",
+    'province': "",
+    'city': "",
+    'county': "",
+    'town': "",
+    'village': "",
+    'detail': "",
+  } 
+  text = ""
+
+
+  # 提取电话
+  phones = jio.extract_phone_number(address_string)
+  for phone in phones:
+     address_string = address_string.replace(phone, " ")
+
+  obj = merge_dicts(obj, process_phones(phones))
+  
+  address = jio.parse_location(address_string, town_village, change2new)
+
+  obj['province'] = address.get('province') if address and address.get('province') is not None else ""
+  obj['city'] = address.get('city') if address and address.get('city') is not None else ""
+  obj['county'] = address.get('county') if address and address.get('county') is not None else ""
+  obj['town'] = address.get('town') if address and address.get('town') is not None else ""
+  obj['village'] = address.get('village') if address and address.get('village') is not None else ""
+  obj['detail'] = address.get('detail')
+  
+  text = address_string.replace(obj.get('province'),'').replace(obj.get('city'),'').replace(obj.get('county'),'').replace(obj.get('town'),'').replace(obj.get('village'),'')
+  
+  obj['name'] = find_name(text,obj)
+  # 清理详情信息中的名字
+  obj['detail'] = address.get('detail').replace(obj.get('name'),"").strip()
+
+  return obj
+
+def merge_dicts(dict1, dict2):
+    merged_dict = dict1.copy()
+    merged_dict.update(dict2)
+    return merged_dict
+
+def process_phones(phones):
+    if len(phones) == 1:
+        return {'phone': phones[0]}
+    else:
+        phone_dict = {f'phone{i}' if i > 0 else 'phone': phone for i, phone in enumerate(phones)}
+        return phone_dict
+
+def find_name(text, obj):
+  name_list = []
+
+  # 使用jieba进行分词
+  seg_list = jieba.cut(text, cut_all=False)
+  for word in seg_list:
+    names = pseg.cut(word)
+    for name in names:
+      if extract_text_before_nr(name) is not None:
+        name_list.append({'value':extract_text_before_nr(name)})
+
+  # 计算名字的相似度
+  for item in name_list:
+     similarity_score = calculate_similarity(item.get('value'), obj.get('province')+obj.get('city')+obj.get('county')+obj.get('town')+obj.get('village')+obj.get('province'))
+     item['similarity_score'] = similarity_score
+  if name_list:
+    min_element = min(name_list, key=lambda x: x['similarity_score'])
+    return min_element.get('value')
+  else:
+    return ''
+
+
+def extract_text_before_nr(input_text):
+    # 定义正则表达式
+    pattern = re.compile(r'(.+)/nr')
+
+    # 使用正则表达式匹配文本
+    match = pattern.search(str(input_text))
+
+    # 如果匹配成功，返回匹配到的文本
+    if match:
+        return match.group(1)
+    else:
+        return None
+
+def calculate_similarity(str1, str2):
+    # 计算 Levenshtein 距离
+    distance = Levenshtein.distance(str1, str2)
+
+    # 计算相似度（值越小越相似）
+    similarity = 1 - distance / max(len(str1), len(str2))
+
+    return similarity
+
+
+def extract_phone_numbers(text):
+    # 手机号正则表达式
+    mobile_pattern = re.compile(r'1[3456789]\d{9}')
+    
+    # 座机号正则表达式（简化版，仅匹配数字和横杠）
+    landline_pattern = re.compile(r'\d{3,4}[-]?\d{7,8}')
+
+    # 提取手机号
+    mobile_numbers = re.findall(mobile_pattern, text)
+
+    # 提取座机号
+    landline_numbers = re.findall(landline_pattern, text)
+
+    return {
+        'mobile_numbers': mobile_numbers,
+        'landline_numbers': landline_numbers
+    }
+
+# 获取命令行参数
+args = sys.argv
+arg1 = args[1] if len(args) > 1 else ''
+port = int(args[2]) if len(args) > 2 and args[2].isdigit() else 3000
+
+
+app = Flask(__name__)
+
+@app.route('/smart_address', methods=['POST'])
+def handle_smart_address():
+    data = request.get_json()
+
+    text = data.get('text', '')
+    town_village = data.get('town_village', True)
+    change2new = data.get('change2new', False)
+    result = smart_address(text, town_village, change2new)
+
+    if result:
+        return jsonify(result)
+    else:
+        return jsonify({"error": "Failed to process the request"}), 500
+
+if __name__ == '__main__' and arg1 == 'serve':
+    app.run(host='0.0.0.0', port=port)
+else:
+   print(smart_address(arg1))
+   
